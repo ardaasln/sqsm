2d902bb41df2b7b2a2796b8175b30dd6dcd66304
safaozturk93 <safaozturk93@gmail.com>
2018-12-29 19:26:47 +0300

    refactor(sqsm): formatting

--- sqsm.py
+++ sqsm.py
@@ -4,13 +4,17 @@ import boto3
 
 
 class Sqsm:
-    def __init__(self, source_queue: str, target_queue: str, profile: str="default"):
-        self.logger = logging.Logger("Sqsm")
-        self.logger.addHandler(logging.StreamHandler())
+    def __init__(self, source_queue: str, target_queue: str, profile: str = "default"):
+        self._logger = logging.Logger("Sqsm")
+        self._logger.addHandler(logging.StreamHandler())
         self._session = boto3.session.Session(profile_name=profile)
         self._client = self._session.client("sqs")
-        self._source_queue = self._client.get_queue_url(QueueName=source_queue).get("QueueUrl")
-        self._target_queue = self._client.get_queue_url(QueueName=target_queue).get("QueueUrl")
+        self._source_queue = self._client.get_queue_url(QueueName=source_queue).get(
+            "QueueUrl"
+        )
+        self._target_queue = self._client.get_queue_url(QueueName=target_queue).get(
+            "QueueUrl"
+        )
 
     def move_messages(self, delete: bool = False):
         while True:
@@ -23,37 +27,41 @@ class Sqsm:
                 self._delete_messages(self._source_queue, _messages)
 
     def _receive_messages(self, queue_url: str) -> list:
-        resp = self._client.receive_message(
-            QueueUrl=queue_url,
-            MaxNumberOfMessages=10,
-            WaitTimeSeconds=10
+        _resp = self._client.receive_message(
+            QueueUrl=queue_url, MaxNumberOfMessages=10, WaitTimeSeconds=10
         )
-        messages = resp.get("Messages", [])
-        self.logger.debug("received {} message(s) from {}".format(len(messages), queue_url))
+        messages = _resp.get("Messages", [])
+        self._logger.debug(
+            "received {} message(s) from {}".format(len(messages), queue_url)
+        )
         return messages
 
     def _delete_messages(self, queue_url: str, messages):
-        entries = [{"Id": message["MessageId"], "ReceiptHandle": message["ReceiptHandle"]} for message in messages]
+        entries = [
+            {"Id": message["MessageId"], "ReceiptHandle": message["ReceiptHandle"]}
+            for message in messages
+        ]
         if len(entries) > 0:
-            self.logger.debug("will remove {} message(s) from {}".format(len(entries), queue_url))
+            self._logger.debug(
+                "will remove {} message(s) from {}".format(len(entries), queue_url)
+            )
             self._client.delete_message_batch(QueueUrl=queue_url, Entries=entries)
         else:
-            self.logger.debug("no message to remove from queue")
+            self._logger.debug("no message to remove from queue")
 
     def _send_messages(self, queue_url: str, messages: list):
-        queue_type = "fifo" if queue_url.endswith(".fifo") else "standard"
-        for message in messages:
-            if queue_type == "fifo":
+        _queue_type = "fifo" if queue_url.endswith(".fifo") else "standard"
+        for _message in messages:
+            if _queue_type == "fifo":
                 unique_id = str(uuid.uuid4())
                 self._client.send_message(
                     QueueUrl=queue_url,
-                    MessageBody=message["Body"],
+                    MessageBody=_message["Body"],
                     MessageGroupId=unique_id,
-                    MessageDeduplicationId=unique_id
+                    MessageDeduplicationId=unique_id,
                 )
             else:
                 self._client.send_message(
-                    QueueUrl=queue_url,
-                    MessageBody=message["Body"]
+                    QueueUrl=queue_url, MessageBody=_message["Body"]
                 )
-        self.logger.debug("sent {} message to {}".format(len(messages), queue_url))
+        self._logger.debug("sent {} message to {}".format(len(messages), queue_url))
